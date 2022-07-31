00b7b8607c3ed7b325333029eb6f8523cf925147
Lourival Sousa <lourivalsousadev@gmail.com>
2022-07-31 18:55:51 -0300

    migrations das tabelas de noticia e users feitas

---
+++ database/migrations/2022_07_31_213113_add_foreign_key_in_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Noticias', function (Blueprint $table) {
+            $table->bigInteger('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Noticias', function (Blueprint $table) {
+            $table->dropForeign('ID_Usuario');
+            $table->dropColumn('id_users');
+        });
+    }
+};
